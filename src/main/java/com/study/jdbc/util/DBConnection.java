f791f69be7f541cb3926328edf681a8a63cc3983
LeeYeongin <youngin5757@gmail.com>
2022-12-15 12:16:07 +0900

    DBConnection

--- src/main/java/com/study/jdbc/util/DBConnection.java
+++ src/main/java/com/study/jdbc/util/DBConnection.java
@@ -27,19 +27,19 @@ public class DBConnection {
 		String password = null;
 		
 		try {
-			Class.forName(Driver.class.getName()); // Class(Driver객체)를 찾아서 객체 생성
+			Class.forName(Driver.class.getName()); // Class(Driver객체)를 찾아서 객체 생성 -> mySQL driver load
 			//Class.forName("com.mysql.cj.jdbc.Driver"); 이런식으로도 사용가능
 			System.out.println("데이터베이스 드라이브 로딩 성공!");
-			url = "jdbc:mysql://localhost:3306/subquery_study"; // database이름
+			url = "jdbc:mysql://localhost:3306/subquery_study"; // 프로토콜//port번호/database이름
 			username = "root";
 			password = "root";
 			
 			connection = DriverManager.getConnection(url, username, password);
 			
-		} catch (ClassNotFoundException e) { //Class.forName()에 필요한 try/catch
+		} catch (ClassNotFoundException e) { //Class.forName()에 필요한 try/catch -> 찾고자하는 경로가 존재하지 않았을때
 			e.printStackTrace();
 			System.out.println("드라이버 로딩 실패!");
-		} catch (SQLException e) { //DriverManager.getConnection(url, username, password)에 필요한 try/catch
+		} catch (SQLException e) { //DriverManager.getConnection(url, username, password)에 필요한 try/catch -> 연결되지 않았을때
 			e.printStackTrace();
 			System.out.println("데이터베이스 연결 실패!");
 		}
